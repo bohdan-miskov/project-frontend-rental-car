d109ecdce97dba110542bdaf8e0ab2a9b5985ed4
Bohdan Miskov <bohdanmiskov@gmail.com>
2025-09-11 22:56:04 +0300

    Add base configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'modern-normalize.css';
+import './assets/styles/styles.css';
+import App from './App/App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import ReactModal from 'react-modal';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
+
+ReactModal.setAppElement('#root');
